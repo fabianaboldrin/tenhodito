62aabff71808187c984acdc4b69206b9c4f5fa7d
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-12-29 18:38:46 -0200

    Added initial classifier to separate protocol to content
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

---
+++ src/tenhodito/nlp/cache.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+import shelve
+import os
+
+
+db = shelve.open(os.path.join(settings.BASE_DIR, 'cache.db'))
+
+
+def load_from_cache(var_name, initial_value_method=None, force_update=False):
+    """Loads a variable from cache.db. If this variable does not exist
+    `initial_value_method` is called and the return is used to set a intial
+    to the variable on cache.
+
+    If the variable name isn't in cache and anyone  initial value method is
+    provided, return None.
+    """
+    try:
+        if force_update:
+            raise KeyError
+        else:
+            return db[var_name]
+    except KeyError:
+        if callable(initial_value_method):
+            db[var_name] = initial_value_method()
+            db.sync()
+            return db[var_name]
+        else:
+            return None
+
+
+def update_cache(var_name, value):
+    """Update a variable with a new value or add a new variable to cache."""
+    db[var_name] = value
+    db.sync()
---
+++ src/tenhodito/nlp/management/commands/classify_speeches.py
@@ -0,0 +1,112 @@
+import json
+import os
+
+from click import secho
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from nlp import cache
+from plagiarism import tokenize, bag_of_words
+from pygov_br.django_apps.camara_deputados.models import Speech
+from textblob.classifiers import NaiveBayesClassifier as Classifier
+from unidecode import unidecode
+
+
+def extractor(text):
+    tokens = tokenize(unidecode(text.casefold()), 'stems',
+                      language='portuguese')
+    features = dict(bag_of_words(tokens, 'bool'))
+    features['?'] = '?' in text
+    features['!'] = '!' in text
+    return features
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        paragraphs = cache.load_from_cache('paragraphs', self._get_paragraphs,
+                                           force_update=True)
+        secho('%s paragraphs' % len(paragraphs), bold=True)
+        self.text_examples = self._load_initial_texts()
+        self.protocol_classifier = cache.load_from_cache(
+            'protocol_classifier', self._protocol_classifier)
+
+        secho('Building bests examples', bold=True)
+        protocols_contents = self.build_best_examples(paragraphs)
+
+        secho('%d protocols and %d contents initially classified' %
+              (len(protocols_contents['protocol']),
+               len(protocols_contents['content'])), bold=True)
+
+        secho('Training the classifier', bold=True)
+        self.protocol_classifier = self.train_classifier(
+            self.protocol_classifier, protocols_contents, 100)
+        cache.update_cache('protocol_classifier', self.protocol_classifier)
+
+    def build_best_examples(self, paragraphs):
+        protocols_contents = cache.load_from_cache('protocols_contents')
+
+        if protocols_contents is None:
+            protocols_contents = {'protocol': [], 'content': []}
+
+        if protocols_contents['protocol'] == [] \
+           or protocols_contents['content'] == []:
+
+            for paragraph in paragraphs:
+                prob_dist = self.protocol_classifier.prob_classify(paragraph)
+                classification = prob_dist.max()
+                probability = prob_dist.prob(classification)
+
+                if probability > 0.8:
+                    secho(classification.upper(), bold=True)
+                    secho('%s\n\n' % paragraph)
+                    protocols_contents[classification].append((probability,
+                                                               paragraph))
+                    protocols_contents[classification].sort(reverse=True)
+            cache.update_cache('protocols_contents', protocols_contents)
+        return protocols_contents
+
+    def train_classifier(self, classifier, training_set, training_set_size):
+        training_set = self._get_training_set(training_set, training_set_size)
+        for idx in range(1, training_set_size + 1):
+            secho("%s) Differences:" % idx)
+            for label, data in training_set.items():
+                try:
+                    old_prob, paragraph = data.pop()
+                    pdf = classifier.prob_classify(paragraph)
+                    if pdf.prob(label) > 0.95:
+                        fmt = (label, 1 - old_prob, 1 - pdf.prob(label))
+                        secho("%s: from %.1e to %.1e" % fmt)
+                        classifier.update([(paragraph, label)])
+                        self._sort_training_set(training_set, classifier)
+                except IndexError:
+                    continue
+        return classifier
+
+    def _get_training_set(self, classes_dict, size):
+        training_set = {}
+        for key, value in classes_dict.items():
+            training_set[key] = value[:size]
+        return training_set
+
+    def _sort_training_set(self, training_set, classifier):
+        for key in training_set.keys():
+            training_set[key].sort(key=lambda x: classifier.prob_classify(x[1])
+                                   .prob(key))
+
+    def _protocol_classifier(self):
+        return Classifier([
+            (self.text_examples['protocol_example'], 'protocol'),
+            (self.text_examples['content_example'], 'content'),
+        ], feature_extractor=extractor)
+
+    def _load_initial_texts(self):
+        examples_file = open(os.path.join(settings.BASE_DIR,
+                                          'nlp/classify_text_examples.json'))
+        return json.load(examples_file)
+
+    def _get_paragraphs(self):
+        data = Speech.objects.all().values_list('full_text', flat=True)
+        paragraphs = []
+        for speech in data:
+            paragraphs.extend(speech.splitlines())
+        return sorted(set(paragraphs))
