f9a5a1736c24fea590d9932535f44a16e2139702
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-04-17 20:37:04 -0300

    Initial component structure
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

---
+++ tenhodito/assets/js/components/application/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Application extends Component {
+  render() {
+    return (
+      <div className="tenhodito-application">
+        Olar
+      </div>
+    );
+  }
+}
+
+export default Application
---
+++ tenhodito/assets/js/index.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Application from './components/application';
+
+ReactDOM.render(<Application />, document.getElementById('container'))
